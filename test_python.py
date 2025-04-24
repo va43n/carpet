abfa86fe5a5491982e9219e9c43ae8407be6cff7
va43n <va43nplay@gmail.com>
2025-04-24 18:27:59 +0700

    Пока что единственным размером картинки для задания является 1920 на 1080, если картинка задания имеет другое расширение (например, 1280 на 720), важно, чтобы координаты центра и радиусы фигур были домножены на нужный коеффициент (в данном случае на 1.5)

--- test_python.py
+++ test_python.py
@@ -1,11 +1,8 @@
-while (True):
-    break
-    arr = [0, 1, 2, 3, 4, 5]
+import numpy as np
 
-    arr.pop(2)
+x, w = 1000, 600
+step = 900
 
-    if arr[0] == 0 and arr[1] == 1 and arr[2] == 3 and arr[3] == 4 and arr[4] == 5:
-        print("ok")
-    else:
-        print(arr, "ne ok")
-        break
+x_span = list(map(int, np.arange(x, x + w, step)))
+
+print(x_span)
